c19c94133c4e5fc5b947dfe48f0ce36bda2dda3a
ulrikpaghschultz <ups@mmmi.sdu.dk>
2016-09-26 15:17:47 +0200

    variable declarations

--- evaluator.js
+++ evaluator.js
@@ -1,28 +1,47 @@
 var exports = module.exports = {};
 var esprima = require('esprima')
 
-function handleBody(body) {
-    return body.map(handle).map(function (x) { return x+";" }).join();
+function handleBody(body,env) {
+    var reduced = [];
+    for(var stmt of body) {
+	var s = handle(stmt,env);
+	if(s!="") reduced.push(s);
+    }
+    return reduced.map(function (x) { return x+";" }).join('');
 }
-function handleProgram(p) {
-    return handleBody(p.body);
+function handleProgram(p,env) {
+    return handleBody(p.body,env);
 }
-function handleExpressionStatement(e) {
-    return handle(e.expression);
+function handleExpressionStatement(e,env) {
+    return handle(e.expression,env);
 }
-function handleBinaryExpression(e) {
-    var left = handle(e.left);
-    var right = handle(e.right);
+function handleBinaryExpression(e,env) {
+    var left = handle(e.left,env);
+    var right = handle(e.right,env);
     if(!isNaN(left) && !isNaN(right)) 
 	return binaryOperatorHandlers[e.operator](left,right)
     else
 	return ""+left+e.operator+right;
 }
-function handleLiteral(x) {
+function handleLiteral(x,env) {
     return x.value;
 }
-function handleIdentifier(x) {
-    return x.name;
+function handleIdentifier(x,env) {
+    if(env[x.name]==undefined)
+	return x.name;
+    else
+	return env[x.name];
+}
+function handleVariableDeclaration(d,env) {
+    var reduced = [];
+    for(var decl of d.declarations) {
+	var init = handle(decl.init,env);
+	if(!isNaN(init))
+	    env[decl.id.name] = init;
+	else
+	    reduced.push(decl.id.name+" = "+init);
+    }
+    return reduced.join(', ');
 }
 
 var binaryOperatorHandlers = {
@@ -34,15 +53,16 @@ var syntaxHandlers = {
     'ExpressionStatement': handleExpressionStatement,
     'BinaryExpression': handleBinaryExpression,
     'Literal': handleLiteral,
-    'Identifier': handleIdentifier
+    'Identifier': handleIdentifier,
+    'VariableDeclaration': handleVariableDeclaration
 }
-function handle(ast) {
+function handle(ast,env) {
     if(syntaxHandlers[ast.type]==undefined)
-	console.log("Undefined handler for type "+ast.type);
-    return syntaxHandlers[ast.type](ast);
+	console.log("Undefined handler for type "+ast.type+", keys: "+Object.keys(ast));
+    return syntaxHandlers[ast.type](ast,env);
 }
 exports.process = function(program) {
-    return handle(esprima.parse(program));
+    return handle(esprima.parse(program),{});
 }
 
 
